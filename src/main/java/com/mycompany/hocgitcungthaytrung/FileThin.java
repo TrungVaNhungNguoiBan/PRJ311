03d389a8c8549f4cfff089288a3d9e32f59a5161
thinhtanpham <thinhtri.2611@gmail.com>
2020-06-27 11:04:49 +0700

    hoan thanh

---
+++ src/main/java/com/mycompany/hocgitcungthaytrung/FileThin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hocgitcungthaytrung;
+
+/**
+ *
+ * @author Rog
+ */
+public class FileThin {
+    private int i;
+    public static void main(String[] args) {
+        System.out.println("Hello mọi người");
+        
+    }
+    
+    public void func(){
+        
+    }
+}
